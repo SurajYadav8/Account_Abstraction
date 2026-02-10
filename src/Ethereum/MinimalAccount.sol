b8f29800a93a75a7c44cd36579f1702651b26f7e
SurajYadav8 <raosahab1e@gmail.com>
2026-02-10 18:20:09 +0000

    lib setup

--- src/Ethereum/MinimalAccount.sol
+++ src/Ethereum/MinimalAccount.sol
@@ -1,7 +1,8 @@
 //SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.24;
+import {IAccount} from "lib/account-abstraction/contracts/interfaces/IAccount.sol";
 
-contract MinimalAccount {
+contract MinimalAccount is IAccount {
 
 }
